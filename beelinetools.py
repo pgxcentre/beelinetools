a71ee64e0db863f42c610d38694687aab8f695d9
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-07-06 08:29:10 -0400

    New version

--- beelinetools.py
+++ beelinetools.py
@@ -15,7 +15,7 @@ from tempfile import NamedTemporaryFile
 __author__ = "Louis-Philippe Lemieux Perreault"
 __copyright__ = "Copyright 2015, Beaulieu-Saucier Pharmacogenomics Centre"
 __license__ = "MIT"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 # The location tuple
