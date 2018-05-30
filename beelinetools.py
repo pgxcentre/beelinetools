db3cb7d49d54228d3a963cf8c5f2ed71eb53745a
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2018-05-30 13:51:33 -0400

    Changed the version

--- beelinetools.py
+++ beelinetools.py
@@ -27,7 +27,7 @@ except ImportError:
 __author__ = "Louis-Philippe Lemieux Perreault"
 __copyright__ = "Copyright 2015, Beaulieu-Saucier Pharmacogenomics Centre"
 __license__ = "MIT"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 
 # The location tuple
